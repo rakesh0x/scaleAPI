adb4b813db26bd7d5bbbce45e7b3e7d0fbd85f91
Rakes <niu-24-14815@niu.edu.in>
2026-08-19 17:50:41 +0530

    serving file in http

--- main.go
+++ main.go
@@ -4,10 +4,28 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"sync"
 )
+
+// mutex for security to shared state
+var counter int
+var mutex = &sync.Mutex{}
+
+func echoString(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "hello")
+}
+
+func incrementCounter(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	counter++
+	fmt.Fprint(w, strconv.Itoa(counter))
+	mutex.Unlock()
+}
 
 func homepage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the landing Page!")
+
 	fmt.Println("Endpoint hit: Homepage")
 }
 
@@ -19,6 +37,10 @@ func dashboard(w http.ResponseWriter, r *http.Request) {
 func handleRequest() {
 	http.HandleFunc("/homepage", homepage)
 	http.HandleFunc("/dashboard", dashboard)
+	http.HandleFunc("/increment", incrementCounter)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, r.URL.Path[1:])
+	})
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
 
