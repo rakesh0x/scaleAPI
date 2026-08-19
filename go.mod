21f4c5c6e5f9a972133f446009db11ccb7c9236e
Rakes <niu-24-14815@niu.edu.in>
2026-08-19 19:05:31 +0530

    refactored with mux

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module myapp
 
 go 1.25.5
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,27 +1,33 @@
 package main
-
 import (
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
-	"sync"
+	"github.com/gorilla/mux"
 )
 
+// import (
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// 	"strconv"
+// 	"sync"
+// )
+
 // mutex for security to shared state
-var counter int
-var mutex = &sync.Mutex{}
+// var counter int
+// var mutex = &sync.Mutex{}
 
-func echoString(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "hello")
-}
+// func echoString(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprint(w, "hello")
+// }
 
-func incrementCounter(w http.ResponseWriter, r *http.Request) {
-	mutex.Lock()
-	counter++
-	fmt.Fprint(w, strconv.Itoa(counter))
-	mutex.Unlock()
-}
+// func incrementCounter(w http.ResponseWriter, r *http.Request) {
+// 	mutex.Lock()
+// 	counter++
+// 	fmt.Fprint(w, strconv.Itoa(counter))
+// 	mutex.Unlock()
+// }
 
 func homepage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the landing Page!")
@@ -34,17 +40,33 @@ func dashboard(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint hit: Dashboard")
 }
 
-func handleRequest() {
-	http.HandleFunc("/homepage", homepage)
-	http.HandleFunc("/dashboard", dashboard)
-	http.HandleFunc("/increment", incrementCounter)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, r.URL.Path[1:])
-	})
-	http.HandleFunc("/article", returnAllArticle)
-	log.Fatal(http.ListenAndServe(":3000", nil))
+// func handleRequest() {
+// 	http.HandleFunc("/homepage", homepage)
+// 	http.HandleFunc("/dashboard", dashboard)
+// 	http.HandleFunc("/increment", incrementCounter)
+// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+// 		http.ServeFile(w, r, r.URL.Path[1:])
+// 	})
+// 	http.HandleFunc("/article", returnAllArticle)
+// 	log.Fatal(http.ListenAndServe(":3000", nil))
+// }
+
+// func main() {
+// 	handleRequest()
+// }
+
+func handleRequests() {
+	myrouter := mux.NewRouter().StrictSlash(true)
+	myrouter.HandleFunc("/", homepage)
+	myrouter.HandleFunc("/article", returnAllArticle)
+
+	log.Fatal(http.ListenAndServe(":3000", myrouter))
 }
 
 func main() {
-	handleRequest()
+	Articles = []Article{
+		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"}, 
+		Article{Title: "Hello 1", Desc: "Article Description 1", Content: "Article Content 1"},
+	}
+	handleRequests()
 }
